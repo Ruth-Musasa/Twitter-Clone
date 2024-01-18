40c96525dbe4ecf51663bc8faad3212560f0f398
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-01-18 17:47:06 +0200

    amelioration du css dans tweetAction

--- src/component/ContendTrend.jsx
+++ src/component/ContendTrend.jsx
@@ -1,18 +1,18 @@
 export default function ContendTrend(props) {
     return (
-        <div className='border border-zinc-800 bg-zinc-800 rounded-lg mt-4 w-full p-4 '>
+        <div className='border border-zinc-800 bg-zinc-800 rounded-lg mt-4 w-full '>
             <div className="flex justify-between">
                 <div
-                    className="text-xl font-bold"> <h2>{props.name}</h2>
+                    className="text-xl font-bold p-4 "> <h2>{props.name}</h2>
                 </div>
-                <div>
+                <div className="p-4">
                     {(props.icon) && <button type="button"><img src="src/assets/Icons/Settings.svg" alt="Photo_icon" /></button>}
                     {(!props.icon) && <></>}
                 </div>
             </div>
             {props.children}
             <div className="pt-4 text-sky-500">
-                <button className="text-base font-bold hover:text-blue-800">Show more</button>
+                <button className="text-base font-bold hover:text-blue-800 p-4 ">Show more</button>
             </div>
         </div>
     )
